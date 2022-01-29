59e3da2e88ffb9ef1b7815a73603637fbe1d4501
Vahid Rafiei <61577988+Vahidr32@users.noreply.github.com>
2022-01-30 01:38:46 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 import os 
 from distutils.dir_util import copy_tree
 import shutil
+
 CL = os.path.dirname(os.path.abspath(__file__))
-DSSWAT=os.path.dirname(CL)+'/SWAT-MODFLOW-RT3D'
+
+with open (CL+'/setup.txt') as file:
+    lines=file.readlines()
+    DMOD=lines[0][25:-1]
+    SWATMODFLOWHPC=lines[2][26:-1]
+    DSSWAT=lines[1][29:-1]
 
 with open(DSSWAT+'/file.cio','r') as file:
     lines=file.readlines()
@@ -13,14 +19,14 @@ with open(DSSWAT+'/file.cio','w') as file:
     for i in range(len(lines)):
         file.write(lines[i])
 
-DMOD=os.path.dirname(CL)+'/MODFLOW'
-SWATMODFLOWHPC=os.path.dirname(CL)+'/SWAT-MODFLOW-HPC'
+
+
 os.mkdir(SWATMODFLOWHPC)
 copy_tree(DSSWAT, SWATMODFLOWHPC,preserve_mode=0)
 SWATMODFLOWPSO=SWATMODFLOWHPC+'/SWAT-MODFLOW-PSO'
 os.mkdir(SWATMODFLOWPSO)
 copy_tree(CL+'/ProgramFiles/Initializationfiles', SWATMODFLOWPSO,preserve_mode=0)
-Baseflow=SWATMODFLOWPSO+'/Baseflow'
+Baseflow=SWATMODFLOWPSO+'/baseflow'
 os.mkdir(Baseflow)
 copy_tree(CL+'/ProgramFiles/baseflow', Baseflow,preserve_mode=0)
 CA_PSO=SWATMODFLOWPSO+'/CA-PSO'
@@ -36,4 +42,4 @@ os.mkdir(Particles)
 MODFLOW=Particles+'/MODFLOW'
 os.mkdir(MODFLOW)
 copy_tree(DMOD, MODFLOW,preserve_mode=0)
-print('the SWAT-MODFLOW-HPC is ready to be transfered to your HPC account'
+print('the SWAT-MODFLOW-HPC is ready to be transfered to your HPC account')
